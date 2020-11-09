df5c5ac19cf5161d98eff6ae3f85236b18ce733a
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-11-10 01:34:21 +0300

    добавить footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ function App() {
 
         <About/>
       </main>
-      {/*<Footer/>*/}
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './Footer.css';
-import git from '../../images/github.svg';
-import code from '../../images/codewars-icon.svg';
 
 function Footer() {
 
@@ -10,23 +8,24 @@ function Footer() {
       <p className="footer__copyright">
         &copy; 2020 Supersite, Powered by News API
       </p>
-      <nav className="footer__navigation">
-        <ul className="footer__links">
+      <nav className="footer__navigation footer__navigation_type_text">
+        <ul className="footer__links footer__links_type_text">
           <li className="footer__list">
             <a href="/" className="footer__link">Главная</a>
           </li>
           <li className="footer__list">
             <a href="https://praktikum.yandex.ru" className="footer__link">Яндекс.Практикум</a>
           </li>
+        </ul>
+      </nav>
+      <nav className="footer__navigation footer__navigation_type_icon">
+        <ul className="footer__links">
           <li className="footer__list">
-            <a href="https://github.com/alexandrprokhorov1988" className="footer__link">
-              <img className="footer__social-icon" src={git} alt="Github"/>
-            </a>
+            <a href="https://github.com/alexandrprokhorov1988" className="footer__link footer__link_type_git"> </a>
           </li>
           <li className="footer__list">
-            <a href="https://www.codewars.com/users/Alexandr%20Prokhorov" className="footer__link">
-              <img className="footer__social-icon" src={code} alt="Codewars"/>
-            </a>
+            <a href="https://www.codewars.com/users/Alexandr%20Prokhorov"
+               className="footer__link footer__link_type_fb"> </a>
           </li>
         </ul>
       </nav>
