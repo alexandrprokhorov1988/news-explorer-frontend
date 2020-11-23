5267817c8be0a6433c55af70aa874792db3af3a4
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-11-23 22:32:44 +0300

    feat: savednews header category description

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -7,20 +7,35 @@ function SavedNewsHeader(
   { loggedIn, titles, onSignIn, onSignOut, isPopupOpen, savedCards }) {
 
   const currentUser = React.useContext(CurrentUserContext);
+  const [category, setCategory]= React.useState([]);
 
   React.useEffect(() => {
     function getPopularCategory() {
       console.log(savedCards);
+      const categoryCountObj = savedCards.reduce((acc, cur, i, arr)=>{
+        if(acc[cur.keyword]){
+          acc[cur.keyword] += 1;
+        } else {
+         acc[cur.keyword] = 1;
+        }
+        return acc;
+      }, {});
+      console.log(categoryCountObj);
 
-      // for()
-      const category = savedCards;
-      if(category.length <= 3){
+      const arrNames = Object.keys(categoryCountObj).sort((a, b) => {
+          return categoryCountObj[b] - categoryCountObj[a];
+      });
+      console.log(arrNames);
+      setCategory(arrNames);
+
+
+      if(arrNames.length <= 3){
 
       } else {
-
       }
-      return {};
+
     }
+
     getPopularCategory();
   }, [savedCards]);
 
@@ -39,9 +54,12 @@ function SavedNewsHeader(
           {`${currentUser.name}, у вас ${savedCards.length} сохранённых статей`}
         </h2>
         <p className="saved-news-header__subtitle">
-          По ключевым словам: <span className="saved-news-header__span-accent">Природа</span>,
-          <span className="saved-news-header__span-accent"> Тайга</span> и
-          <span className="saved-news-header__span-accent"> 2-м другим</span>
+          По ключевым словам: <span className="saved-news-header__span-accent"> {category[0] || ''}</span>,
+          <span className="saved-news-header__span-accent"> {category[1] || ''}</span> и
+          {category.length <= 3 ?
+            <span className="saved-news-header__span-accent"> {category[2]}</span> :
+            <span className="saved-news-header__span-accent"> {category.length - 2}-м другим</span>
+          }
         </p>
       </div>
     </header>
